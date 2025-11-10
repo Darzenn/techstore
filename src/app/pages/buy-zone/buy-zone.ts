210cbdbc2db0d7513b1f4102d46206bc52dc717a
Sebastian Tafur <eliottxt650@gmail.com>
2025-11-10 02:07:21 -0500

    Agregando Zona de Pago

--- src/app/pages/buy-zone/buy-zone.ts
+++ src/app/pages/buy-zone/buy-zone.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartService, CartItem } from '../../services/cart';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-buy-zone',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './buy-zone.html',
   styleUrl: './buy-zone.css',
 })
-export class BuyZone {
 
+export class BuyZone {
+  items: CartItem[] = [];
+  total = 0;
+  pedidoConfirmado = false;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    // Obtenemos los productos actuales del carrito
+    this.cartService.cart$.subscribe(cart => {
+      this.items = cart;
+      this.total = this.cartService.getTotal();
+    });
+  }
+
+  confirmarPedido() {
+    this.pedidoConfirmado = true;
+    this.cartService.clearCart(); // vacía el carrito después de confirmar
+
+    // Simulamos una "espera de procesamiento"
+    setTimeout(() => {
+      this.pedidoConfirmado = false;
+      alert('✅ ¡Tu pedido fue confirmado con éxito!');
+    }, 2000);
+  }
 }
